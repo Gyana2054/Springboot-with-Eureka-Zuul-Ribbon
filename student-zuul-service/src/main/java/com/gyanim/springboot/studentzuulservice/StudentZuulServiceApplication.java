9a7855a8c352e5f42975922aad83b1a50c1da199
Gyana Ranjan Pattnaik <gyana.pattnaik20@gmail.com>
2019-01-21 22:01:09 +0530

    student-zuul-service-master

---
+++ student-zuul-service/src/main/java/com/gyanim/springboot/studentzuulservice/StudentZuulServiceApplication.java
@@ -0,0 +1,44 @@
+package com.gyanim.springboot.studentzuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.gyanim.springboot.studentzuulservice.filter.ErrorFilter;
+import com.gyanim.springboot.studentzuulservice.filter.PostFilter;
+import com.gyanim.springboot.studentzuulservice.filter.PreFilter;
+import com.gyanim.springboot.studentzuulservice.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class StudentZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentZuulServiceApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
+}
+
